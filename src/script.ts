741e4343bb40e39548023262528e0394890a9f0e
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-09-30 13:58:54 -0300

    teste import

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,5 @@
+import * as teste from './soma';
+
 // para campos html (inputs), é necessario definilos como abaixo
 // o restante se mantem da mesma forma que o javascript
 let num1 = document.getElementById('numero1') as HTMLInputElement;
@@ -7,10 +9,7 @@ let res = document.getElementById('result');
 
 // ao passar paramentros para uma função é necessario identificar
 // o tipo do valor a ser recebido
-function soma(num1: number, num2: number){
-    return num1 + num2;
-}
 
 button?.addEventListener('click', () => {
-    res!.innerHTML = soma(+num1.value, +num2.value).toString();
+    res!.innerHTML = teste.soma(+num1.value, +num2.value).toString();
 });
