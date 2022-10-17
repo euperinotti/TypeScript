a74c184c9fb73a234ba22b10666bce95dd0fe427
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-17 12:00:02 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 // rodando um servidor com node 
 
-<<<<<<< HEAD
-import express, { Request, Response} from 'express';
-=======
 import express, { Request, Response } from 'express';
 import mainRoutes from '../routes/routes';
->>>>>>> 8cf44bff10fe75990db0cb28a2a632ed63271ee2
 
 const server = express();
 
@@ -13,13 +9,9 @@ server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!');
 })
 
-<<<<<<< HEAD
-server.listen(3000)
-=======
 server.use(mainRoutes);
 server.use((req: Request, res: Response) => {
     res.status(404).send('Página não encontrada');
 })
 
 server.listen(3000);
->>>>>>> 8cf44bff10fe75990db0cb28a2a632ed63271ee2
