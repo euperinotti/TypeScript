1cbe2be24edf156e76eede752d45a6bf81ced85b
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-19 23:41:00 -0300

    pastas estaticas

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
-// rodando um servidor com node 
+// importando as bibliotecas ---------- 
 
 import express, { Request, Response } from 'express';
+
+// define um diretorio fixo para o servidor
+import path from 'path';
+
 import mainRoutes from '../routes/routes';
 
+// configurando o serivdor ----------
+
+// instanciando o servidor
 const server = express();
 
 server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!');
 })
 
+// Os arquivos presentes dentro desse diretorio estarao acessiveis
+// por meio de url
+server.use(express.static(path.join(__dirname,'../public')));
+
 server.use(mainRoutes);
+
 server.use((req: Request, res: Response) => {
     res.status(404).send('Página não encontrada');
 })
 
-server.listen(3000);
+server.listen(3000); // porta do servidor
