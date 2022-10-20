0510a7980ca35df633620e6febdaf0f4000e61a1
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-19 23:53:13 -0300

    configurando o mustache

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,17 @@ import express, { Request, Response } from 'express';
 
 // define um diretorio fixo para o servidor
 import path from 'path';
-
+import mustache from 'mustache-express';
 import mainRoutes from '../routes/routes';
 
 // configurando o serivdor ----------
 
 // instanciando o servidor
 const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname,'views'));
+server.engine('mustache', mustache());
 
 server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!');
