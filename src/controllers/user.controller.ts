c384979ac0e0d0c7f02c6499c6a877791469e919
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-26 19:43:20 -0300

    update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import User from '../models/User';
 
-let peri: User = {
-    name: 'Guilherme',
-    lastName: 'Perinotti',
-    age: 19,
-    showWelcome: true
+export const Users = (req: Request, res: Response) => {
+    let peri: User = {
+        name: 'Guilherme',
+        lastName: 'Perinotti',
+        age: 19,
+        showWelcome: true
+    }
+
+    res.render('pages/home', {
+        user: peri
+    });
 }
