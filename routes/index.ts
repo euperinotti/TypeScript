29559bc2b60bb0639787846d5a20a0678c93b186
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-26 13:40:50 -0300

    introducao a mvc

--- routes/index.ts
+++ routes/index.ts
@@ -1,38 +1,6 @@
 import { Router, Request, Response } from 'express';
-import Product from '../src/class/product'
 
 const router = Router();
-
-class User {
-    name: string;
-    lastName: string;
-    age: number;
-    showWelcome: boolean;
-
-    constructor(name: string, lastName: string, age: number){
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-        this.showWelcome = false || true;
-    }
-}
-
-let peri: User = {
-    name: 'Guilherme',
-    lastName: 'Perinotti',
-    age: 19,
-    showWelcome: true
-}
-
-let produto1: Product = {
-    name: 'maca',
-    price: 10
-}
-
-let produto2: Product = {
-    name: 'banana',
-    price: 15
-}
 
 router.get('/', (req: Request, res: Response) => {
     res.render('pages/home', {
