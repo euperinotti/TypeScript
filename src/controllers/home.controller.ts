fa1fd0a2cacc7a00b18726f59e6bcd7c7e183673
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-11-20 15:42:02 -0300

    inserindo dados com build

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -6,28 +6,36 @@ import { User } from '../models/User';
 export const home = async (req: Request, res: Response) => {
     let users = await User.findAll({
         where: {
-            // nome: {
-            //     [Op.like]: '%a%'
-            // },
-            // idade: {
-            //     // [Op.gt]: 18, -> Maior que
-            //     // [Op.gte]: 18, -> Maior ou igual
-            //     // [Op.lt]: 20, -> Menor que
-            //     [Op.lte]: 20 // Menor ou igual a
-            //     // [Op.between]: [10, 20] -> Retorna dados que estejam entre 10 e 20
-            // }
-
+            nome: {
+                [Op.like]: '%o%'
+            },
+            idade: {
+                // [Op.gt]: 18, -> Maior que
+                // [Op.gte]: 18, -> Maior ou igual
+                // [Op.lt]: 20, -> Menor que
+                [Op.lte]: 50 // Menor ou igual a
+                // [Op.between]: [10, 20] -> Retorna dados que estejam entre 10 e 20
+            }
             // [Op.or]: [ // Condicoes de busca
             //     { nome: 'tiago' },
             //     { idade: 20 }
             // ]
-        }, order: [
+        },
+        order: [
             ['idade', 'DESC'], // ASC ou DESC
             ['nome', 'ASC']
         ],
         offset: 1, // Pula x itens da consulta
         limit: 3 // Limita a consulta a 3 itens apenas
     })
+
+    let newUser = User.build({
+        nome: 'Giovane',
+        sobrenome: 'Comelli',
+        idade: 19
+    });
+
+    newUser.save();
 
     console.log(JSON.stringify(users));
     // try{
